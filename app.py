41039fc44f96fa7fd2f96133b10af81640e9caaf
Vinicius de Melo <114265969+viniciusdmelo@users.noreply.github.com>
2023-03-15 15:58:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
    return "Olá, mundo! Esse é o meu site de teste. Prazer, sou Vinícius."
+
+@app.route("/sobre")
+def sobre():
+   return "Aqui vai o conteúdo da página Sobre"
+
+@app.route("/contato")
+def contato():
+   return "Aqui vai o conteúdo da página Contato"
