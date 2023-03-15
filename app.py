0a2fa315bdb685158e4e0c2994f4f9567c7d73a9
Vinicius de Melo <114265969+viniciusdmelo@users.noreply.github.com>
2023-03-15 16:19:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+menu = """
+<a href="/">Página incial</a> |
+<a href="/sobre">Sobre</a> |
+<a href="/contato">Contato</a>
+<br>
+"""
+
 @app.route("/")
 def index():
-   return "Olá, mundo! Esse é o meu site de teste. Prazer, sou Vinícius."
+   return menu + "Olá, mundo! Esse é o meu site de teste. Prazer, sou Vinícius."
 
 @app.route("/sobre")
 def sobre():
-   return "Aqui vai o conteúdo da página Sobre"
+   return menu + "Este é um site de teste feito durante a aula de Algoritmos de Automação"
 
 @app.route("/contato")
 def contato():
-   return "Aqui vai o conteúdo da página Contato"
+   return menu + "Você pode encontrar em contato comigo pelo @viniciusdmelo nas redes sociais"
