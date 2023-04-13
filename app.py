6b54ae8ec3f358d9c39a31da5b28192942ae121b
Vinicius de Melo <114265969+viniciusdmelo@users.noreply.github.com>
2023-04-13 17:19:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def promocoes():
 def dedoduro():
   mensagem = {"chat_id": TELEGRAM_ADMIN_ID, "text": "Alguém acessou a página dedo duro!"}
   resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem)
-  return f"Mensagem enviada. Resposta: ({resposta.status_code}) - {resposta.text}"
+  return f"Mensagem enviada. \n\nResposta: ({resposta.status_code}) - {resposta.text}"
 
 @app.route("/telegram-bot", methods=["POST"])
 def telegram_bot():
